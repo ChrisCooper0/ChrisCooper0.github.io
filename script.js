5869591e93540524de288a15f81532e9ca417f37
Chris Cooper <chriscooper@Chriss-MacBook-Air.local>
2021-04-18 13:43:53 +0100

    Updated copy

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle("active");
 });
+
+//AOS Library
+AOS.init();
